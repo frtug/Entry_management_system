db3a373015cbd2fbf6d628ad4319c0c51570e119
Abhishek Sharma <abhishek11703182@gmail.com>
2019-11-28 16:43:18 +0530

    2nd commit

--- module1.py
+++ module1.py
@@ -39,31 +39,31 @@ user_phone = input("enter the phone number")
 now = datetime.now()
 check_in = now.strftime("%H:%M:%S")
 
-print("Current Time =", check_in)
+print("Current Time =",check_in)
 
 host_name = input("Enter Your Name")
 host_email = input("enter the Email")
 host_phone = input("enter the phone number")
 
-sub = "Checking In User"
-msg = "Mr "+host_name +", "+ user_name +" is coming"
+sub = "Checking In"
+msg = user_name + "\n" + user_phone + " \n" + user_email
 send_email(sub,msg,host_email)
 
-address_visted = "BH5 Room No 820,LPU  "
+address_visted = "BH5 Room No 820,LPU PUNJAB "
 out = input("Press Q or q to checkout\n")
 if out == 'Q' or out =='q' :
     nowout = datetime.now()
     check_out = nowout.strftime("%H:%M:%S")
     print(check_out)
-    sub = "Checking Out User"
-    msg = user_name + " is leaving \n" + user_phone + " \n" + user_email +"\n check in "+check_in+"\n   check out "+check_out;
-    send_email(sub, msg,host_email)
+    sub = "Checking Out"
+    msg = user_name + " \n" + user_phone + " \n" + user_email +"\n check in "+check_in+"\n   check out "+check_out+"\n Thank you fo the visit sir";
+    send_email(sub, msg,user_email)
 
 con.execute("INSERT INTO COMPANY VALUES(?,?,?,?,?,?)",(user_name,user_phone,check_in,
                                                    check_out,host_name,
                                                        address_visted))
 con.commit()
-print("VAlues are inserted")
+print("Values are inserted")
 
 d = input("Hit Y or y to access the stored data or press any key to exit")
 if d == "y" or d == "Y":
